5d2cf9ffd7022b005e5b47ecac1806549f2dd57c
Admin-PC\Admin <len88ok@gmail.com>
2016-09-13 16:02:48 +0300

    hometask 5 allmost done

--- hometask5/js/main.js
+++ hometask5/js/main.js
@@ -1,3 +1,9 @@
-$(function() {
-    // code..
-})
+$(document).ready(function() {
+ 	  	$(".owl-carousel").owlCarousel({	 
+	      nav : false, // Show next and prev buttons
+	      dots: true,
+	      slideSpeed : 300,
+	      singleItem:true,
+	      items : 1
+	 	  });	
+		});
