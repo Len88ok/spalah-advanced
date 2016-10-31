697c1f016c86d217a7876ec6fdb90f4a205e0c38
Admin-PC\Admin <len88ok@gmail.com>
2016-10-31 16:49:24 +0200

    sticky nav and sections without drop

---
+++ hometask9-2/dist/scripts/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  var affixElement = '#navbar-main';
+
+  $(affixElement).affix({
+    offset: {
+      // Distance of between element and top page
+      top: function () {
+        return (this.top = $(affixElement).offset().top)
+      },
+      // when start #footer 
+      bottom: function () { 
+        return (this.bottom = $('#footer').outerHeight(true))
+      }
+    }
+  });
+});
+
